b431467cb7a2aefe49096ae3448e1ef848f21bf7
Wojciech Krawczyk <wojciech.krawczyk@schibsted.pl>
2015-09-14 18:36:09 +0200

    Catch login/logout events

--- web/index.php
+++ web/index.php
@@ -6,12 +6,25 @@
   <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
   <script src="https://payment.schibsted.no/js/spid-sdk-1.7.9.min.js"></script>
   <script>
-  // Add event subscribers
-  VGS.Event.subscribe('auth.login', function(data) { console.log(data); });
-  VGS.Event.subscribe('auth.logout', function(data) { console.log(data); });
-  VGS.Event.subscribe('auth.sessionChange', function(data) { console.log(data); });
+
+  VGS.Event.subscribe('auth.login', function(data) {
+      $("#logout").show();
+      $("#login").hide();
+      $("#logout a").attr("href", VGS.getLogoutURI() )
+      $("#user a").text(data.session.displayName);
+    });
   
-  //Initiate SDK
+  VGS.Event.subscribe('auth.logout', function(data) {
+      $("#logout").hide();
+      $("#login").show();
+      $("#login a").attr("href", VGS.getLoginURI() )
+      $("#user a").text("Not logged in");
+    });
+    
+  VGS.Event.subscribe('auth.sessionChange', function(data) {
+    console.log(data);
+    });
+  
   VGS.init({
     client_id: "55f6e4c1efd8c056041fbe75",
     server: "payment.schibsted.no"
@@ -22,10 +35,10 @@
   <h1>STP SPiD Test</h1>
   <dl>
     <dt>Options</dt>
-    <dd><a href="">Login</a></dd>
-    <dd><a href="">Logout</a></dd>
+    <dd id="login"><a href="">Login</a></dd>
+    <dd id="logout"><a href="">Logout</a></dd>
     <dt>User</dt>
-    <dd></dd>
+    <dd id="user"></dd>
   </dl>
 </body>
 </html>
