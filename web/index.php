53400427d06166e0d0dad95ee168a8fb1e7126ef
Wojciech Krawczyk <wojciech.krawczyk@schibsted.pl>
2015-09-15 21:57:36 +0200

    Code formating

--- web/index.php
+++ web/index.php
@@ -6,64 +6,67 @@
   <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
   <script src="https://payment.schibsted.no/js/spid-sdk-1.7.9.min.js"></script>
   <script>
-  $(document).ready(function() {
-    function handleLoginUsers(data) {
-      $("#logout").show();
-      $("#login").hide();
-      $("#signup").hide();
-      $("#logout a").attr("href", VGS.getLogoutURI() );
-      $("#logout a").click( function() {
-        VGS.Auth.logout( function() { document.location.reload(); } );
-      });
-      $("#user").text(data.session.displayName);
-    }
+    $(document).ready(function () {
+      function handleLoginUsers(data) {
+        $("#logout").show();
+        $("#login").hide();
+        $("#signup").hide();
+        $("#logout a").attr("href", VGS.getLogoutURI());
+        $("#logout a").click(function () {
+          VGS.Auth.logout(function () {
+            document.location.reload();
+          });
+        });
+        $("#user").text(data.session.displayName);
+      }
 
-    function handleLogoutUsers(data) {
-      $("#logout").hide();
-      $("#login").show();
-      $("#signup").show();
-      $("#login a").attr("href", VGS.getLoginURI() )
-      $("#signup a").attr("href", VGS.getSignupURI() )
-      $("#user").text("Not logged in");
-    }
+      function handleLogoutUsers(data) {
+        $("#logout").hide();
+        $("#login").show();
+        $("#signup").show();
+        $("#login a").attr("href", VGS.getLoginURI())
+        $("#signup a").attr("href", VGS.getSignupURI())
+        $("#user").text("Not logged in");
+      }
 
-    VGS.Event.subscribe('auth.login', function(data) {
+      VGS.Event.subscribe('auth.login', function (data) {
         console.log("auth.login", data);
         handleLoginUsers(data)
       });
 
-    VGS.Event.subscribe('auth.logout', function(data) {
+      VGS.Event.subscribe('auth.logout', function (data) {
         console.log("auth.logout", data);
         handleLogoutUsers(data);
       });
 
-    VGS.Event.subscribe('auth.sessionChange', function(data) {
+      VGS.Event.subscribe('auth.sessionChange', function (data) {
         console.log("auth.sessionChange", data);
         var sess = data.session || null;
-        if( sess ) {
+        if (sess) {
           handleLoginUsers(data)
         } else {
           handleLogoutUsers(data);
         }
       });
 
-    VGS.init({
-      client_id: "55f6e4c1efd8c056041fbe75",
-      server: "payment.schibsted.no"
+      VGS.init({
+        client_id: "55f6e4c1efd8c056041fbe75",
+        server: "payment.schibsted.no"
+      });
     });
-  });
   </script>
 </head>
 <body>
-  <h1>STP SPiD Test</h1>
-  <h2>JavaScript</h2>
-  <dl>
-    <dt>Options</dt>
-    <dd id="signup"><a href="">Signup</a></dd>
-    <dd id="login"><a href="">Login</a></dd>
-    <dd id="logout"><a href="">Logout</a></dd>
-    <dt>User</dt>
-    <dd id="user">Unknown</dd>
-  </dl>
+<h1>STP SPiD Test</h1>
+
+<h2>JavaScript</h2>
+<dl>
+  <dt>Options</dt>
+  <dd id="signup"><a href="">Signup</a></dd>
+  <dd id="login"><a href="">Login</a></dd>
+  <dd id="logout"><a href="">Logout</a></dd>
+  <dt>User</dt>
+  <dd id="user">Unknown</dd>
+</dl>
 </body>
 </html>
